70e4cf1a9815aec90c749e5c10955e3e78d3f1a6
BaMoustapha <127317481+BaMoustapha@users.noreply.github.com>
2023-07-26 08:04:53 +0000

    Add files via upload

---
+++ op/index.js
@@ -0,0 +1,7 @@
+let numberOfSeasons = 6;
+let numberOfEpisodes = 12;
+
+// ==========================================
+
+let paragraph = document.querySelector('#info');
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
---
+++ op/objet/index.js
@@ -0,0 +1,9 @@
+let episodeTitle = 'The First Battle';
+let episodeDuration = 45;
+let hasBeenWatched = false;
+
+// =========================================
+
+document.querySelector('#episode-info').innerText = `Episode: ${episodeTitle}
+Duration: ${episodeDuration} min
+${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
